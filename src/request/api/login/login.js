c222cd9e9d98244b3c09c53f123815b8bf2cbd00
TuoCangYu <2367314268@qq.com>
2022-03-09 09:09:14 +0800

    fix(http): 修改get请求params携带参数的函数命名

--- src/request/api/login/login.js
+++ src/request/api/login/login.js
@@ -1,4 +1,4 @@
-import { getQuery, getRestful, post, put, remove } from '@/request/http.js'
+import { getQuery, getParams, post, put, remove } from '@/request/http.js'
 
 export const login = (params) => {
   return post('/login', params)
